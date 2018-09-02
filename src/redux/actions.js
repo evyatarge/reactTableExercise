b7cb394d2aeb2cc3702fb4abdb4a0e742ab5ce56
evyatarge <evyatarge@gmail.com>
2018-09-02 14:42:59 +0300

    remove unnecessary files

--- src/redux/actions.js
+++
@@ -1,6 +0,0 @@
-//actions types
-export const SAVE_DATA = 'SAVE_DATA';
-
-export function saveData(data) {
-    return { type: SAVE_DATA, data }
-}
--- src/table/Row.js
+++ src/table/Row.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import './row.css';
-
 
 class Row extends Component{
 
